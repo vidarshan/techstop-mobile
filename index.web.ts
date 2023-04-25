c0ca7308365547fd80e5991e6d3650a2d010fac6
Vidarshan <vidarshanadithya3@gmail.com>
2023-04-25 21:01:35 +0530

    Remove commented code

--- index.web.ts
+++ index.web.ts
@@ -8,8 +8,3 @@ AppRegistry.runApplication(appName, {
   initialProps: {},
   rootTag: document.getElementById('app-root'),
 });
-window.open(
-  'https://booking-app-tu3v.onrender.com',
-  `Techstop Mobile`,
-  'resizable=no,width=420,height=870',
-);
